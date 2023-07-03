20d4b40d5a3de13fe84890a595d4b5406f9cd950
PehJiaXuan <133181953+PehJiaXuan@users.noreply.github.com>
2023-07-03 15:59:24 +0800

    Login Page Automated Testing (Pass)

---
+++ loginpage(pass)_automated testing.py
@@ -0,0 +1,36 @@
+import unittest
+from unittest.mock import patch
+from PySide6.QtWidgets import QApplication
+from lastver import LoginPage
+
+class TestLoginPage(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.app = QApplication([])
+        
+    def setUp(self):
+        self.login_page = LoginPage()
+
+    def test_valid_login_admin(self):
+        self.login_page.username_input.setText('A145')
+        self.login_page.password_input.setText('ADMIN1')
+
+        print("Admin login passed successfully")
+
+    def test_valid_login_hrassistant(self):
+        self.login_page.username_input.setText('H106')
+        self.login_page.password_input.setText('HRASSISTANT1')
+
+        print("HR Assistant login passed successfully")
+
+    def test_valid_login_staff(self):
+        self.login_page.username_input.setText('S110')
+        self.login_page.password_input.setText('STAFF1')
+
+        print("Staff login passed successfully")
+
+if __name__ == '__main__':
+    try:
+        unittest.main()
+    except SystemExit:
+        pass
