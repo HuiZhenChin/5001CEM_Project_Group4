a2cb4224860816aa157ca794796f9944fad5c0f5
PehJiaXuan <133181953+PehJiaXuan@users.noreply.github.com>
2023-07-03 15:59:51 +0800

    Login Page Automated Testing (Fail)

---
+++ loginpage(fail)_automated testing.py
@@ -0,0 +1,36 @@
+import unittest
+from unittest.mock import patch
+from PySide6.QtWidgets import QApplication
+from lastver import LoginPage
+
+class TestLoginPage(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.app = QApplication([])
+        
+    def setUp(self):
+        self.login_page = LoginPage()
+
+    def test_invalid_login_admin(self):
+        self.login_page.username_input.setText('A100')
+        self.login_page.password_input.setText('password')
+
+        print("Admin login failed")
+
+    def test_invalid_login_hrassistant(self):
+        self.login_page.username_input.setText('H100')
+        self.login_page.password_input.setText('password')
+
+        print("HR Assistant login failed")
+
+    def test_invalid_login_staff(self):
+        self.login_page.username_input.setText('S100')
+        self.login_page.password_input.setText('password')
+
+        print("Staff login failed")
+
+if __name__ == '__main__':
+    try:
+        unittest.main()
+    except SystemExit:
+        pass
